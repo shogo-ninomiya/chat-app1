059631e39a9e5251fc65d60d47a56fd75426a264
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-08-22 17:24:50 +0900

    再度更新

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,10 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image
+<<<<<<< Updated upstream
   validates :content, presence: true, unless: :was_attached?
+=======
+>>>>>>> Stashed changes
 
   def was_attached?
     self.image.attached?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2020_08_22_075206) do
+=======
+ActiveRecord::Schema.define(version: 2020_08_22_074151) do
+>>>>>>> Stashed changes
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
