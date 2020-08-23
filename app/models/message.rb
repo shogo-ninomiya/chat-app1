09b3a9884cba208d6ed996bd1f1f4dc1539ef318
shogo-ninomiya <s.ninomiya19930831@gmail.com>
2020-08-23 16:45:42 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,10 +2,7 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image
-<<<<<<< Updated upstream
   validates :content, presence: true, unless: :was_attached?
-=======
->>>>>>> Stashed changes
 
   def was_attached?
     self.image.attached?
